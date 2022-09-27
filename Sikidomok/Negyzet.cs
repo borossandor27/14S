1b9e9d2f8132f48537f5ad0646fb12f2feb06465
Boros Sándor <boros.sandor@dszcberegszaszi.hu>
2022-09-27 10:57:41 +0200

    Add project files.

---
+++ Sikidomok/Negyzet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sikidomok
+{
+    internal class Negyzet : Sikidom
+    {
+        double oldal;
+
+        public double Oldal { get => oldal; set => oldal = value; }
+
+        public Negyzet(double oldal)
+        {
+            Oldal = oldal;
+        }
+
+        public override double Kerulet()
+        {
+            return oldal * 4;
+        }
+
+        public override double Terulet()
+        {
+            return oldal * oldal;
+        }
+    }
+}
---
+++ Sikidomok/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sikidomok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Sikidom> sikidoms = new List<Sikidom>();
+
+            sikidoms.Add(new Kor(2.5));
+            sikidoms.Add(new Kor(5.1));
+            sikidoms.Add(new Negyzet(2.5));
+            sikidoms.Add(new Negyzet(4.1));
+
+            foreach (Sikidom item in sikidoms)
+            {
+                string tipus = "";
+                if (item.GetType().Equals(typeof(Kor)))
+                {
+                    tipus = "Kör";
+                }
+                else if (item.GetType().Equals(typeof(Negyzet)))
+                {
+                    tipus = "Négyzet";
+                }
+                Console.WriteLine($"{tipus}\tkerület: {item.Kerulet():N2} \tterület: {item.Terulet():N2}");
+            }
+
+            Console.WriteLine("\nProgram vége!");
+            Console.ReadKey();
+        }
+    }
+}
